eb2e73ce2627d190cadb3975b7dc130eda4c95b9
Jonathan Yin <jonathanyin.12@gmail.com>
2019-09-14 16:01:11 -0400

    Create scatter_plot.py

---
+++ scatter_plot.py
@@ -0,0 +1,40 @@
+from bokeh.plotting import output_file, show, ColumnDataSource
+from bokeh.io import output_notebook
+from bokeh.palettes import Category20
+
+def plot_mols(x, y, labels, imgs, groups, s=8, alpha = 0.8, x_label='', y_label='', title='', size=(800,600)):  
+    TOOLTIPS = """
+        <div>
+            <div>
+                <span style="font-size: 12px; font-weight: bold;">@name</span>
+            </div>
+            <div>
+                <img src="@imgs" height="100" alt="@imgs" border="1.5"></img>
+            </div>
+        </div>
+    """
+    p = figure(plot_width=size[0], 
+               plot_height=size[1],  
+               tooltips=TOOLTIPS, 
+               title=title,
+               x_axis_label=x_label, 
+               y_axis_label=y_label)
+    
+    if len(np.unique(groups))<=2:
+        colors= Category20[3]
+    else:
+        colors = Category20[len(np.unique(groups))]
+
+    points = ColumnDataSource(data=dict(
+       x=x,
+       y=y,
+       name=labels,
+       imgs=imgs,
+       color=[colors[x] for x in groups]
+    ))
+        
+    p.circle('x', 'y',  fill_color='color',line_color=None, size=s, alpha = alpha, source=points)
+
+    output_notebook()
+    output_file("toolbar.html")
+    return p
